f1b3cd3c41fafc62e274cb7bef40859aafdc68a4
Mat Schmid <mathieu.schmid@carleton.ca>
2018-09-21 14:06:56 -0400

    TEST: Added Hand Identifying tests: Three of a kind

---
+++ src/test/java/IdentifyHands/IdentifyThreeOfAKindTests.java
@@ -0,0 +1,42 @@
+package IdentifyHands;
+
+import junit.framework.TestCase;
+import Model.Card;
+import Strategy.HandIdentifier;
+
+public class IdentifyThreeOfAKindTests extends TestCase {
+	
+	public void testThreeJacks() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(11, 1); //JD
+		hand[1] = new Card(11, 2); //JC
+		hand[2] = new Card(11, 3); //JH
+		hand[3] = new Card(1, 4); //AS
+		hand[4] = new Card(5, 1); //5D
+		
+		assertTrue(HandIdentifier.isThreeOfAKind(hand));
+	}
+	
+	public void testThreeSixes() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(6, 1); //6D
+		hand[1] = new Card(6, 2); //6C
+		hand[2] = new Card(6, 3); //6H
+		hand[3] = new Card(1, 4); //AS
+		hand[4] = new Card(5, 1); //5D
+		
+		assertTrue(HandIdentifier.isThreeOfAKind(hand));
+	}
+	
+	public void testFakeThreeOfAKind() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(11, 1); //JD
+		hand[1] = new Card(1, 2); //AC
+		hand[2] = new Card(9, 3); //9H
+		hand[3] = new Card(1, 4); //AS
+		hand[4] = new Card(5, 1); //5D
+		
+		assertFalse(HandIdentifier.isThreeOfAKind(hand));
+	}
+	
+}
