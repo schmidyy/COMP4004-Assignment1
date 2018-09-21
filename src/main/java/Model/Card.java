89fdf8a27d821f9dedbc87ce3af773d23eb4978b
Mat Schmid <mathieu.schmid@carleton.ca>
2018-09-21 09:44:47 -0400

    Implementation: Added Card model class

---
+++ src/main/java/Model/Card.java
@@ -0,0 +1,105 @@
+package Model;
+
+// Basic Card class taken from:
+// https://docs.oracle.com/javase/tutorial/java/javaOO/examples/Card.java
+
+public final class Card {
+	private final int rank;
+    private final int suit;
+
+    // Kinds of suits
+    public final static int DIAMONDS = 1;
+    public final static int CLUBS    = 2;
+    public final static int HEARTS   = 3;
+    public final static int SPADES   = 4;
+
+    // Kinds of ranks
+    public final static int ACE   = 1;
+    public final static int DEUCE = 2;
+    public final static int THREE = 3;
+    public final static int FOUR  = 4;
+    public final static int FIVE  = 5;
+    public final static int SIX   = 6;
+    public final static int SEVEN = 7;
+    public final static int EIGHT = 8;
+    public final static int NINE  = 9;
+    public final static int TEN   = 10;
+    public final static int JACK  = 11;
+    public final static int QUEEN = 12;
+    public final static int KING  = 13;
+
+    public Card(int rank, int suit) {
+        assert isValidRank(rank);
+        assert isValidSuit(suit);
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+    
+    public String toString() {
+    	return String.format("%s%s", Card.rankToString(this.rank), Card.suitToString(this.suit));
+    }
+
+    public static boolean isValidRank(int rank) {
+        return ACE <= rank && rank <= KING;
+    }
+
+    public static boolean isValidSuit(int suit) {
+        return DIAMONDS <= suit && suit <= SPADES;
+    }
+
+    public static String rankToString(int rank) {
+        switch (rank) {
+        case ACE:
+            return "A";
+        case DEUCE:
+            return "2";
+        case THREE:
+            return "3";
+        case FOUR:
+            return "4";
+        case FIVE:
+            return "5";
+        case SIX:
+            return "6";
+        case SEVEN:
+            return "7";
+        case EIGHT:
+            return "8";
+        case NINE:
+            return "9";
+        case TEN:
+            return "10";
+        case JACK:
+            return "J";
+        case QUEEN:
+            return "Q";
+        case KING:
+            return "K";
+        default:
+            return null;
+        }    
+    }
+    
+    public static String suitToString(int suit) {
+        switch (suit) {
+        case DIAMONDS:
+            return "♦";
+        case CLUBS:
+            return "♣";
+        case HEARTS:
+            return "♥";
+        case SPADES:
+            return "♠";
+        default:
+            return null;
+        }    
+    }
+}
