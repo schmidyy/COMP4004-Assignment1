58f757d19a80a3f5fc29438a45a37d968355111d
Mat Schmid <mathieu.schmid@carleton.ca>
2018-09-22 19:41:46 -0400

    TEST: Added Exchange Startegy tests: Straight or better

---
+++ src/test/java/ExchangeStrategy/E0StraightOrBetterTests.java
@@ -0,0 +1,87 @@
+package ExchangeStrategy;
+
+import junit.framework.TestCase;
+import Model.Card;
+import Model.ExchangeResponse;
+
+public class E0StraightOrBetterTests extends TestCase {
+	
+	public void testRoyalFlushExchangesNoCards() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(10, 1); //10D
+		hand[1] = new Card(11, 1); //JD
+		hand[2] = new Card(12, 1); //QD
+		hand[3] = new Card(13, 1); //KD
+		hand[4] = new Card(1, 1); //AD
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(0, response.getNumExchanges());
+		assertEquals(new boolean[]{false, false, false, false, false}, response.getCardsToExchange());
+	}
+	
+	public void testStraightFlushExchangesNoCards() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(1, 1); //AD
+		hand[1] = new Card(2, 1); //2D
+		hand[2] = new Card(3, 1); //3D
+		hand[3] = new Card(4, 1); //4D
+		hand[4] = new Card(5, 1); //5D
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(0, response.getNumExchanges());
+		assertEquals(new boolean[]{false, false, false, false, false}, response.getCardsToExchange());
+	}
+	
+	public void testFourOfAKindExchangesNoCards() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(1, 1); //AD
+		hand[1] = new Card(2, 1); //2D
+		hand[2] = new Card(3, 1); //3D
+		hand[3] = new Card(4, 1); //4D
+		hand[4] = new Card(5, 1); //5D
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(0, response.getNumExchanges());
+		assertEquals(new boolean[]{false, false, false, false, false}, response.getCardsToExchange());
+	}
+	
+	public void testFullHouseExchangesNoCards() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(1, 1); //AD
+		hand[1] = new Card(2, 1); //2D
+		hand[2] = new Card(3, 1); //3D
+		hand[3] = new Card(4, 1); //4D
+		hand[4] = new Card(5, 1); //5D
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(0, response.getNumExchanges());
+		assertEquals(new boolean[]{false, false, false, false, false}, response.getCardsToExchange());
+	}
+	
+	public void testFlushExchangesNoCards() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(1, 1); //AD
+		hand[1] = new Card(2, 1); //2D
+		hand[2] = new Card(3, 1); //3D
+		hand[3] = new Card(4, 1); //4D
+		hand[4] = new Card(5, 1); //5D
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(0, response.getNumExchanges());
+		assertEquals(new boolean[]{false, false, false, false, false}, response.getCardsToExchange());
+	}
+	
+	public void testStraightExchangesNoCards() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(1, 1); //AD
+		hand[1] = new Card(2, 1); //2D
+		hand[2] = new Card(3, 1); //3D
+		hand[3] = new Card(4, 1); //4D
+		hand[4] = new Card(5, 1); //5D
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(0, response.getNumExchanges());
+		assertEquals(new boolean[]{false, false, false, false, false}, response.getCardsToExchange());
+	}
+	
+}
