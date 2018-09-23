b6bfa98b3688067bcfb6e5f8b98dbfc3ed9d3ef4
Mat Schmid <mathieu.schmid@carleton.ca>
2018-09-23 19:03:36 -0400

    Implementation: Added Exchange coverage: One away - Flush

--- src/main/java/Strategy/Exchange.java
+++ src/main/java/Strategy/Exchange.java
@@ -1,5 +1,8 @@
 package Strategy;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import Model.*;
 import Strategy.HandIdentifier;
 
@@ -9,6 +12,9 @@ public class Exchange {
 		ExchangeResponse response = new ExchangeResponse(null, 0);
 		if (isStraightOrBetter(hand)) {
 			response.setCardsToExchange(new boolean[]{false, false, false, false, false});
+		} else if (isOneAwayFromFlush(hand)) {
+			response.setCardsToExchange(getMissingFlushCard(hand));
+			response.setNumExchanges(1);
 		}
 		return response;
 	}
@@ -22,4 +28,52 @@ public class Exchange {
 				HandIdentifier.isStraight(hand));
 	}
 	
+	private static boolean isOneAwayFromFlush(Card[] hand) {
+		HashMap<Integer, Integer>  cardCount = cardsPerRank(hand);
+		
+		if (cardCount.containsValue(4)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean[] getMissingFlushCard(Card[] hand) {
+		HashMap<Integer, Integer>  cardCount = cardsPerRank(hand);
+		
+		// Find entry with lowest value
+		Entry<Integer, Integer> min = null;
+		for (Entry<Integer, Integer> entry : cardCount.entrySet()) {
+		    if (min == null || min.getValue() > entry.getValue()) {
+		        min = entry;
+		    }
+		}
+		int suit = min.getKey();
+		
+		// Build response array
+		boolean[] cardsToExchange = new boolean[5];
+		for (int i = 0; i < hand.length; i++) {
+			if (hand[i].getSuit() == suit) {
+				cardsToExchange[i] = true;
+			} else {
+				cardsToExchange[i] = false;
+			}
+		}
+		
+		return cardsToExchange;
+	}
+	
+	private static HashMap<Integer, Integer> cardsPerRank(Card[] hand) {
+		// [Card suit : Number of occurrences]
+		HashMap<Integer, Integer>  cardCount = new HashMap<>();
+		for (int i = 0; i < hand.length; i++) {
+			int suit = hand[i].getSuit();
+			Integer count = cardCount.get(suit);
+			if (count == null) {
+				cardCount.put(suit, 1);
+			} else {
+				cardCount.put(suit, count + 1);
+			}
+		}
+		return cardCount;
+	}
 }
