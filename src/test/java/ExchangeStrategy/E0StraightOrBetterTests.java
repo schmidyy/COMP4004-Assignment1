5fdecec25ef7d9a21184edeba2873d99d6643808
Mat Schmid <mathieu.schmid@carleton.ca>
2018-09-23 14:57:47 -0400

    TEST: Added Exchange Startegy tests: One Away

--- src/test/java/ExchangeStrategy/E0StraightOrBetterTests.java
+++ src/test/java/ExchangeStrategy/E0StraightOrBetterTests.java
@@ -1,7 +1,6 @@
 package ExchangeStrategy;
 
 import junit.framework.TestCase;
-
 import java.util.Arrays;
 import Model.Card;
 import Model.ExchangeResponse;
@@ -37,10 +36,10 @@ public class E0StraightOrBetterTests extends TestCase {
 	
 	public void testFourOfAKindExchangesNoCards() {
 		Card[] hand = new Card[5];
-		hand[0] = new Card(1, 1); //AD
-		hand[1] = new Card(2, 1); //2D
-		hand[2] = new Card(3, 1); //3D
-		hand[3] = new Card(4, 1); //4D
+		hand[0] = new Card(4, 1); //4D
+		hand[1] = new Card(4, 2); //4C
+		hand[2] = new Card(4, 3); //4H
+		hand[3] = new Card(4, 4); //4S
 		hand[4] = new Card(5, 1); //5D
 		
 		ExchangeResponse response = Exchange.analyze(hand);
@@ -50,11 +49,11 @@ public class E0StraightOrBetterTests extends TestCase {
 	
 	public void testFullHouseExchangesNoCards() {
 		Card[] hand = new Card[5];
-		hand[0] = new Card(1, 1); //AD
-		hand[1] = new Card(2, 1); //2D
-		hand[2] = new Card(3, 1); //3D
-		hand[3] = new Card(4, 1); //4D
-		hand[4] = new Card(5, 1); //5D
+		hand[0] = new Card(13, 1); //KD
+		hand[1] = new Card(13, 2); //KC
+		hand[2] = new Card(13, 3); //KH
+		hand[3] = new Card(1, 4); //AS
+		hand[4] = new Card(1, 1); //AD
 		
 		ExchangeResponse response = Exchange.analyze(hand);
 		assertEquals(0, response.getNumExchanges());
@@ -63,11 +62,11 @@ public class E0StraightOrBetterTests extends TestCase {
 	
 	public void testFlushExchangesNoCards() {
 		Card[] hand = new Card[5];
-		hand[0] = new Card(1, 1); //AD
-		hand[1] = new Card(2, 1); //2D
-		hand[2] = new Card(3, 1); //3D
-		hand[3] = new Card(4, 1); //4D
-		hand[4] = new Card(5, 1); //5D
+		hand[0] = new Card(5, 2); //5C
+		hand[1] = new Card(7, 2); //7C
+		hand[2] = new Card(3, 2); //3C
+		hand[3] = new Card(9, 2); //9C
+		hand[4] = new Card(10, 2); //10C
 		
 		ExchangeResponse response = Exchange.analyze(hand);
 		assertEquals(0, response.getNumExchanges());
@@ -76,11 +75,11 @@ public class E0StraightOrBetterTests extends TestCase {
 	
 	public void testStraightExchangesNoCards() {
 		Card[] hand = new Card[5];
-		hand[0] = new Card(1, 1); //AD
-		hand[1] = new Card(2, 1); //2D
-		hand[2] = new Card(3, 1); //3D
-		hand[3] = new Card(4, 1); //4D
-		hand[4] = new Card(5, 1); //5D
+		hand[0] = new Card(6, 1); //6D
+		hand[1] = new Card(7, 3); //7H
+		hand[2] = new Card(8, 2); //8C
+		hand[3] = new Card(9, 2); //9C
+		hand[4] = new Card(10, 1); //10D
 		
 		ExchangeResponse response = Exchange.analyze(hand);
 		assertEquals(0, response.getNumExchanges());
---
+++ src/test/java/ExchangeStrategy/E1OneAwayTests.java
@@ -0,0 +1,76 @@
+package ExchangeStrategy;
+
+import java.util.Arrays;
+import Model.Card;
+import Model.ExchangeResponse;
+import Strategy.Exchange;
+import junit.framework.TestCase;
+
+public class E1OneAwayTests extends TestCase {
+	
+	public void testOneAwayFromRoyalFlushExchangesOneCard() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(10, 1); //10D
+		hand[1] = new Card(11, 1); //JD
+		hand[2] = new Card(7, 2); //7C **
+		hand[3] = new Card(13, 1); //KD
+		hand[4] = new Card(1, 1); //AD
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(1, response.getNumExchanges());
+		assertTrue(Arrays.equals(new boolean[]{false, false, true, false, false}, response.getCardsToExchange()));
+	}
+	
+	public void testOneAwayFromStraightFlushExchangesOneCard() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(1, 1); //AD
+		hand[1] = new Card(2, 1); //2D
+		hand[2] = new Card(3, 1); //3D
+		hand[3] = new Card(4, 1); //4D
+		hand[4] = new Card(8, 2); //8C **
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(1, response.getNumExchanges());
+		assertTrue(Arrays.equals(new boolean[]{false, false, false, false, true}, response.getCardsToExchange()));
+	}
+	
+	public void testOneAwayFromFullHouseExchangesOneCard() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(13, 1); //KD
+		hand[1] = new Card(13, 2); //KC
+		hand[2] = new Card(13, 3); //KH
+		hand[3] = new Card(5, 4); //5S **
+		hand[4] = new Card(1, 1); //AD
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(1, response.getNumExchanges());
+		assertTrue(Arrays.equals(new boolean[]{false, false, false, true, false}, response.getCardsToExchange()));
+	}
+	
+	public void testOneAwayFromFlushExchangesOneCard() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(5, 1); //5D
+		hand[1] = new Card(7, 1); //7D
+		hand[2] = new Card(3, 2); //3C **
+		hand[3] = new Card(9, 1); //9D
+		hand[4] = new Card(10, 1); //10D
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(1, response.getNumExchanges());
+		assertTrue(Arrays.equals(new boolean[]{false, false, true, false, false}, response.getCardsToExchange()));
+	}
+	
+	public void testOneAwayFromStraightExchangesOneCard() {
+		Card[] hand = new Card[5];
+		hand[0] = new Card(2, 1); //2D ***
+		hand[1] = new Card(7, 3); //7H
+		hand[2] = new Card(8, 2); //8C
+		hand[3] = new Card(9, 2); //9C
+		hand[4] = new Card(10, 1); //10D
+		
+		ExchangeResponse response = Exchange.analyze(hand);
+		assertEquals(1, response.getNumExchanges());
+		assertTrue(Arrays.equals(new boolean[]{true, false, false, false, false}, response.getCardsToExchange()));
+	}
+	
+}
