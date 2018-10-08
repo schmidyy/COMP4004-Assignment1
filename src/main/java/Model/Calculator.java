8abe2f3a9e47024ec7c6a72bb669103a2906ab20
Mat Schmid <mathieu.schmid@carleton.ca>
2018-10-08 12:23:56 -0400

    Implementation: Added Acceptance test coverage: Winning Tests

--- src/main/java/Model/Calculator.java
+++ src/main/java/Model/Calculator.java
@@ -1,6 +1,10 @@
 package Model;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import Model.Card;
 import Strategy.HandIdentifier;
@@ -16,18 +20,43 @@ public class Calculator {
 	}
 	
 	private static String rankScore(Card[] hand) {
-		int[] ranks = new int[5];
-		for (int i = 0; i < hand.length; i++) {
-			ranks[i] = hand[i].getRank();
-		}
-		Arrays.sort(ranks);
-		
 		String ranksString = "";
-		for (int i = ranks.length - 1; i >= 0; i--) {
-			String formatedString = String.format("%02d", ranks[i]);
+		
+		ArrayList<Integer> pairRanks = new ArrayList<Integer>();
+		ArrayList<Integer> soloRanks = new ArrayList<Integer>();
+		HashMap<Integer, Integer>  cardCount = HandIdentifier.cardsPerRank(hand);
+		
+		for (Entry<Integer, Integer> entry : cardCount.entrySet()) {
+			if (entry.getValue() == 4) {
+				for (int i = 0; i < 4; i++) {
+					ranksString += String.format("%02d", entry.getKey());
+				}
+			} else if (entry.getValue() == 3) {
+				for (int i = 0; i < 3; i++) {
+					ranksString += String.format("%02d", entry.getKey());
+				}
+			} else if (entry.getValue() == 2) {
+				pairRanks.add(entry.getKey());
+			} else {
+				soloRanks.add(entry.getKey());
+			}
+		}
+		
+		Collections.sort(pairRanks);
+		Collections.reverse(pairRanks);
+		Collections.sort(soloRanks);
+		Collections.reverse(soloRanks);
+		
+		for (int i : pairRanks) {
+			String formatedString = String.format("%02d", i);
+			ranksString += formatedString + formatedString;
+		}
+		
+		for (int i : soloRanks) {
+			String formatedString = String.format("%02d", i);
 			ranksString += formatedString;
 		}
-		
+
 		return ranksString;
 	}
 	
